10aacb632b61b33db598a92ce8c7942cb90724fe
medbendadi <itsbendadi@gmail.com>
2022-10-08 05:40:04 +0100

    Adding index.html page

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require("path")
 const productsRoutes = require('./Routes/products')
 const categoriesRoutes = require('./Routes/category')
+const router = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('json spaces', 3);
 
+
 app.use('/', productsRoutes)
 app.use('/', categoriesRoutes)
+
+
+router.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname + '/view/index.html'));
+});
+app.use('/', router);
+
 const server = app.listen(process.env.PORT || 5000, () => {
    console.log('listening on port %s...', server.address().port);
 });
